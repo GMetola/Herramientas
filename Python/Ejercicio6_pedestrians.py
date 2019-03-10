d402cb483bed7fd25a52b34d9605e0e8b8ec12db
Gabriel-Casa <metolag@gmail.com>
2019-03-10 21:44:45 +0100

    Ej6 working v1.1

--- Python/Ejercicio6_pedestrians.py
+++ Python/Ejercicio6_pedestrians.py
@@ -8,7 +8,7 @@ if __name__ == '__main__':
     path = 'F:/Portatil/Vision Artificial/Herramientas/pedestrian_sequence/'
     folder = os.listdir(path)
     img_height, img_width, layers = cv2.imread(path+folder[0]).shape
-### cambiar por lista de tamaño 1000
+# TODO cambiar por lista de tamaño 1000
     im_collection = []
     i = 0
 
@@ -21,14 +21,14 @@ if __name__ == '__main__':
 
         rectangles, weights = hog.detectMultiScale(gray, winStride=(8, 8),
                                                    padding=(32, 32), scale=1.05)
-### reducir el numero de rectangulos. descartar si weights < 1?
+# ToDO reducir el numero de rectangulos. descartar si weights < 1?
         [cv2.rectangle(cap, (x, y), (x + w, y + h), (0, 255, 0), 3) for x, y, w, h in rectangles]
         cv2.imshow('wololo', cap)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
         im_collection.append(cap)
         i += 1
-### incluir barra de progreso
+# todo incluir barra de progreso
         print("Calculating rectangles: ", i * 100 // (len(folder)), "%")
 
     out = cv2.VideoWriter('pedestrian detected.avi',
@@ -37,10 +37,10 @@ if __name__ == '__main__':
     for j in range(len(im_collection)):
         print("Creating video: ", j * 100 // len(im_collection), "%")
         out.write(im_collection[j])
-######## por que no aparece nada con el out.release?
     out.release()
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
+# TODO Cosmin der Große's heavenly tips to turn this shitty code into rocket programming
 
 
